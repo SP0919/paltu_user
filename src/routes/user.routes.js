98801d53938603666c6457def713d704b829c6c3
amanjot-pixlerlab <amanjot.pixlerlab@gmail.com>
2022-08-22 18:03:15 +0530

    Service provider and service type update

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -11,13 +11,13 @@ router.get("/", userController.findAll);
 router.post("/register", upload.single("user_profile"), userController.create);
 router.post("/login", userController.signIn);
 // Retrieve a single user with id
-router.get("/:id", isAuthenticated, upload.none(),userController.findOne);
+router.get("/:id", isAuthenticated, upload.none(), userController.findOne);
 // Update a user with id
-router.put("/:id", isAuthenticated, upload.none(),userController.update);
+router.put("/:id", isAuthenticated, upload.none(), userController.update);
 // Delete a user with id
 router.delete("/:id", userController.delete);
-router.delete("/:id", userController.delete);
-router.post("/change-password",upload.none(), userController.changePassword);
+
+router.post("/change-password", upload.none(), userController.changePassword);
 
 // router.delete("/:id", userController.delete);
 module.exports = router;
